212203f931f0fef27192424f0b53cf058c69a2c6
Costin Gheorghe <cgheorghe0702@stud.acs.upb.ro>
2025-02-07 17:48:38 +0200

    [UPDATE] added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,62 @@
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
-import { Manrope, DM_Sans } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { Manrope, DM_Sans } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
-import './globals.css'
-import { ThemeProvider } from '@/components/theme'
-import ReactQueryProvider from '@/react-query'
-import { ReduxProvider } from '@/redux/provider'
-import { Toaster } from 'sonner'
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme";
+import ReactQueryProvider from "@/react-query";
+import { ReduxProvider } from "@/redux/provider";
+import { Toaster } from "sonner";
+import Script from "next/script";
 
-const manrope = DM_Sans({ subsets: ['latin'] })
+const manrope = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Recordr',
-  description: 'Share AI powered videos with your friends.',
-}
+	title: "Recordr",
+	description: "Share AI powered videos with your friends.",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode
+	children: React.ReactNode;
 }>) {
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`${manrope.className} bg-[#171717]`}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            disableTransitionOnChange
-          >
-            <ReduxProvider>
-              <ReactQueryProvider>
-                {children}
-                <Toaster />
-              </ReactQueryProvider>
-            </ReduxProvider>
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
-  )
+	return (
+		<ClerkProvider>
+			<html lang="en">
+				{/* <!-- Google tag (gtag.js) --> */}
+				<Script
+					id="gtm-script-1"
+					strategy="lazyOnload"
+					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+				/>
+
+				<Script id="gtm-script-2" strategy="lazyOnload">
+					{`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+				</Script>
+				<body className={`${manrope.className} bg-[#171717]`}>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="dark"
+						disableTransitionOnChange
+					>
+						<ReduxProvider>
+							<ReactQueryProvider>
+								{children}
+								<Toaster />
+							</ReactQueryProvider>
+						</ReduxProvider>
+					</ThemeProvider>
+				</body>
+			</html>
+		</ClerkProvider>
+	);
 }
